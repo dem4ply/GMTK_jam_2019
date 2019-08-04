d003faf7c6e54b00f5e20b2a91ab674e31696dfa
Dem4ply <dem4ply@users.noreply.github.com>
2019-08-04 09:10:57 -0500

    delay

--- Assets/Resources/platformer/script/Chibi_event.cs
+++ Assets/Resources/platformer/script/Chibi_event.cs
@@ -10,10 +10,17 @@ namespace platformer.animator
 	public class Chibi_event : chibi.Chibi_behaviour
 	{
 		public bool want_to_invoke = false;
+		public float delay = 2f;
 		public UnityEvent e;
 
 		public void invoke()
 		{
+			StartCoroutine( "__invoke" );
+		}
+
+		public IEnumerator __invoke()
+		{
+			yield return new WaitForSeconds( delay );
 			e.Invoke();
 		}
 
